108a442563361f97d3aa7c3a8589b9e6279df831
ELTHEK-112 <karimelalkaoui@gmail.com>
2020-12-04 19:21:33 +0100

    startin

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+
+
+   namespace App\Controllers; 
+
+
+
+   class indexController  extends BaseController{
+
+                public function show(){
+                    echo" test controler home page ";
+                }
+
+
+   }
